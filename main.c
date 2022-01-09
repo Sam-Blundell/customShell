cae5b63c922d6daba4899e828331a57cd36df6a6
Sam Blundell <samcalblundell@googlemail.com>
2022-01-09 21:11:12 +0000

    add REPL logic to main loop. Necessary read_cmd logic not yet implemented

--- main.c
+++ main.c
@@ -7,9 +7,34 @@
 int main(int argc, char **argv)
 {
 
+    char *cmd;
+    
     do
     {
         print_prompt1();
+
+        cmd = read_cmd();
+
+        if(!cmd)
+        {
+            exit(EXIT_SUCCESS):
+        }
+
+        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+        {
+            free(cmd);
+            continue;
+        }
+
+        if(strcmp(cmd, "exit\n") == 0)
+        {
+            free(cmd);
+            break;
+        }
+
+        printf("%S\n", cmd);
+
+        free(cmd);
 
     } while(1);
 
